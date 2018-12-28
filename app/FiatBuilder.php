7a9e50f50b2873599c1b3ad26155efc1078ebf26
joneng2016 <jonathan_samara@hotmail.com>
2018-12-28 14:54:10 -0200

    FiatBuilder

---
+++ app/FiatBuilder.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace App;
+
+public class FiatBuilder extends CarroBuilder
+{
+    public function buildPreco()
+    {
+        return $this->carro->preco;
+    }
+    public function buildDscMotor()
+    {
+        $this->carro->dscMotor = "Fire Flex 1.0";
+    }
+    public function buildAnoDeFabricacao()
+    {
+        $this->carro->anoDeFabricacao = 2011;
+    }
+    public function buildModelo()
+    {
+        $this->carro->modelo = "Palio";
+    }
+    public function buildMontadora()
+    {
+        $this->carro->montadora = "Fiat";
+    }
+}
